bb97500468a5906ec3fbed4cb75d2ddc8369ff17
har0ke <okehargens@gmail.com>
2020-07-05 21:22:14 +0200

    asdfasdf

--- scripts/dumpall.py
+++ scripts/dumpall.py
@@ -3,7 +3,7 @@ import os
 import sys
 import subprocess
 for i in range(20):
-    number = str(pow(i, 3))
+    number = str(pow(i + 1, 3))
     args = [os.path.join(os.path.dirname(__file__), "test.py"), number] + sys.argv[1:]
     print(" ".join(args))
     subprocess.check_call(args)
